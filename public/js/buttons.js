7dd8fb274e239b615ab6a3313188251f92556e31
Christian Wentling <christianwentling@gmail.com>
2017-05-09 17:18:08 -0400

    with hotel on the map

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -2,7 +2,19 @@ $('#hotel_button').on('click', function(evt){
   var $currentHotelSelected = ($( '#hotel-choices option:selected').text())
   console.log($currentHotelSelected);
   $('#hotelSelection').replaceWith('<span class="title" id="hotelSelection">' + $currentHotelSelected + '</span>');
+  for (var hotelObj in hotels){
+    // console.log(hotelObj);
+    if (hotels[hotelObj].name === $currentHotelSelected){
+      var hotelLocation = (hotels[hotelObj].place.location);
+      drawMarker('hotel', hotelLocation);
+      // initializeMap.drawMarker('hotel', hotelLocation);
+    }
+  }
+  // console.log(.place.location);
 })
+
+
+
 
 // $( "div.second" ).replaceWith( "<h2>New heading</h2>" );
 
